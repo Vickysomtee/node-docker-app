aaa4ff548fed3b950bfa8c6653725349f5df04a6
vickysomtee <vickysomtee@gmail.com>
2021-10-21 01:20:51 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use('/', (req, res, next) => {
+  res.send("Hello There")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
